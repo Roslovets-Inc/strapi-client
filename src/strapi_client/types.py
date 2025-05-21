5c31a4af80df5c04997841831585d50fba9113a1
Pavel Roslovets <p.v.roslovets@gmail.com>
2025-05-21 23:56:26 +0200

    feat: MediaImageDocument type for responsive images

--- src/strapi_client/types.py
+++ src/strapi_client/types.py
@@ -95,3 +95,48 @@ class ApiParameters(BaseModel):
             **(stringify_parameters('publicationState', self.publication_state) if self.publication_state else {}),
             **(stringify_parameters('locale', self.locale) if self.locale else {}),
         }
+
+
+class MediaImageFormatVariant(BaseModel):
+    ext: str
+    url: str
+    hash: str
+    mime: str
+    name: str
+    path: str | None = None
+    size: float = Field(description='Size in kilobytes')
+    width: int
+    height: int
+    size_in_bytes: int = Field(alias='sizeInBytes')
+
+
+class MediaImageFormats(BaseModel):
+    thumbnail: MediaImageFormatVariant | None = None
+    small: MediaImageFormatVariant | None = None
+    medium: MediaImageFormatVariant | None = None
+    large: MediaImageFormatVariant | None = None
+
+    @property
+    def largest(self) -> MediaImageFormatVariant | None:
+        if self.large: return self.large
+        if self.medium: return self.medium
+        if self.small: return self.small
+        if self.thumbnail: return self.thumbnail
+        return None
+
+
+class MediaImageDocument(BaseDocument):
+    name: str
+    alternative_text: str | None = Field(default=None, alias='alternativeText')
+    caption: str | None = None
+    width: int
+    height: int
+    formats: MediaImageFormats
+    hash: str
+    ext: str
+    mime: str
+    size: float = Field(description='Size in kilobytes')
+    url: str
+    preview_url: str | None = Field(default=None, alias='previewUrl')
+    provider: str
+    provider_metadata: dict[str, Any] | None = None
